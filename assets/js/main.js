2fb6cf35e11db1d72328d353cda1ed9d770dd7cc
empschul-leipzig <62986106+empschul-leipzig@users.noreply.github.com>
2023-10-30 10:39:37 +0100

    Add Slider Services.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -283,3 +283,35 @@
 	});
 
 })(jQuery);
+
+$(function() {
+  $('.slider-services').slick({
+    dots: true,
+    appendDots: '.slider-dots',
+    arrows: true,
+		infinite: false,
+		slidesToScroll: 3,
+		slidesToShow: 3,
+		accessibility: true,
+		variableWidth: false,
+		focusOnSelect: false,
+		centerMode: false,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToScroll: 2,
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToScroll: 1,
+					slidesToShow: 1
+				}
+			}
+		]
+  });
+});  
+
